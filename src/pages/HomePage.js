971eeb88aa50a7dbe80e28e9b6d4d0cfa2f62164
Rahul Kumar <cse.170310430@silicon.ac.in>
2022-09-12 02:37:18 +0000

    new: weather card main info added

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,6 +8,11 @@ import { alpha, styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
+import Stack from '@mui/material/Stack';
+import Switch from '@mui/material/Switch';
+import Card from '@mui/material/Card';
+import Grid from '@mui/material/Grid';
+import Avatar from '@mui/material/Avatar';
 function HomePage() {
 	return (
 		<Container component='main' maxWidth='md'>
@@ -34,22 +39,78 @@ function HomePage() {
 					),
 				}}
 			/>
-			<Box
-				sx={{
-					marginTop: 8,
-					display: 'flex',
-					flexDirection: 'column',
-					alignItems: 'center',
-				}}
-			>
-				<Paper variant='outlined' square elevation={6} sx={{}}></Paper>
-			</Box>
+			<WeatherInfoCard />
 		</Container>
 	);
 }
 
 export default HomePage;
 
-function WeatherInfoCard(){
-    
+function WeatherInfoCard() {
+	return (
+		<Card sx={{ marginTop: 2, padding: 2 }}>
+			<Stack
+				direction='row'
+				justifyContent={'space-between'}
+				alignItems='center'
+				sx={{ marginBottom: { xs: 2, sm: 6 } }}
+			>
+				<Typography variant='h5' gutterBottom>
+					Current Weather
+				</Typography>
+				<Stack direction='row' spacing={1} alignItems='center'>
+					<Typography>Off</Typography>
+					<Switch
+						checked={true}
+						inputProps={{ 'aria-label': 'controlled' }}
+					/>
+					<Typography>On</Typography>
+				</Stack>
+			</Stack>
+			<Grid
+				container
+				spacing={{ xs: 2, md: 3 }}
+				columns={{ xs: 4, sm: 8, md: 12 }}
+			>
+				<Grid item xs={4} sm={4} md={6}>
+					<Box>
+						<Typography
+							variant='h6'
+							gutterBottom
+							textAlign={'center'}
+							width='100%'
+						>
+							{'New York'}
+						</Typography>
+						<Stack direction='row' alignItems='center' spacing={2}>
+							<Avatar
+								alt='Remy Sharp'
+								src='http://openweathermap.org/img/wn/10d@2x.png'
+								sx={{ width: 100, height: 100 }}
+							/>
+							<Typography fontSize={'7rem'}>
+								{'45'}&deg;
+							</Typography>
+						</Stack>
+
+						<Typography
+							variant='body1'
+							gutterBottom
+							textAlign={'center'}
+							width='100%'
+						>
+							{'light rain'}
+						</Typography>
+					</Box>
+				</Grid>
+				<Grid item xs={4} sm={4} md={6}>
+					<Typography>{'Feels Like'}</Typography>
+					<Typography>{'High Low'}</Typography>
+					<Typography>{'Wind Speed'}</Typography>
+					<Typography>{'Pressure'}</Typography>
+					<Typography>{'Sun Rise | Sun Set'}</Typography>
+				</Grid>
+			</Grid>
+		</Card>
+	);
 }
